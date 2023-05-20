aed97b3f815d879e09896efe706234907f6b0120
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2023-05-20 20:56:31 +0100

    includes some c libraries and main.c

--- shell.h
+++ shell.h
@@ -2,7 +2,19 @@
 #define _SHELL__
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
 int _env(data_shell *datash);
+
+#endif /* _SHELL__ */
+
+
+
